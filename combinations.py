5bf986d489edfe9fa1bd8e499145f89c24320a8a
Aleksandr Vetrov <vetrov.cnc@gmail.com>
2020-02-13 22:07:55 +0200

    Refactoring combinations.py module

--- combinations.py
+++ combinations.py
@@ -2,25 +2,28 @@ def combinations(iterable, r, r_max, number=None, query=None):
     # combinations('ABCD', 2) --> AB AC AD BC BD CD
     # начинаем генерацию со строк с одним символом
     r = r if not query else len(query)
+    # нумерация сгенерированных комбинаций необходима для того, чтобы
+    # можно было из базы данных получить последнюю комбинацию при многопоточной записи
     number = number if number else 0
-    pool = tuple(iterable)
-    n = len(pool)
+    n = len(iterable)
     if r > n:
         return
+    # приводим к списку т.к. далее будем изменять номера индексов
     indices = list(range(r))
     if not query:
         # первая комбинация без переданной строки query для продолжения
         number += 1
-        yield (number, ''.join(tuple(pool[i] for i in indices)))
+        yield (number, iterable[:r])
     else:
         # если переданная стартовая строка длиннее, чем генерируемые - выходим
-        if len(query) > r:
+        query_len = len(query)
+        if query_len > r:
             return
-        for f in range(len(query)):
+        for f in range(query_len):
             # формируем начальное состояние индексов исходя из стартовой строки query
             indices[f] = iterable.find(query[f])
     while True:
-        for i in reversed(list(range(r))):
+        for i in range(r-1,-1,-1):
             if indices[i] != i + n - r:
                 break
         else:
@@ -32,4 +35,4 @@ def combinations(iterable, r, r_max, number=None, query=None):
         for j in range(i+1, r):
             indices[j] = indices[j-1] + 1
         number += 1
-        yield (number, ''.join(tuple((pool[i] for i in indices))))
+        yield (number, ''.join((iterable[i] for i in indices)))
